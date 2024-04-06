67de004f3f196c6788101096bd6a4b64de96de4e
Jimi <jimi.pihlajakangas@gmail.com>
2024-04-06 18:20:57 +0300

    currency-js

--- public_html/js/Quote_Currency_Project/currency.js
+++ public_html/js/Quote_Currency_Project/currency.js
@@ -1,1 +1,22 @@
-console.log("currency.js is connected");
+document.getElementById('currencyForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent form submission
+
+    // Get user inputs
+    var fromCurrency = document.getElementById('fromCurrency').value;
+    var toCurrency = document.getElementById('toCurrency').value;
+    var amount = document.getElementById('amount').value;
+
+    // API call to perform currency conversion
+    fetch('https://api.exchangerate-api.com/v4/latest/' + fromCurrency)
+        .then(response => response.json())
+        .then(data => {
+            var conversionRate = data.rates[toCurrency];
+            var convertedAmount = amount * conversionRate;
+
+            // Display result
+            document.getElementById('result').innerHTML = amount + ' ' + fromCurrency + ' = ' + convertedAmount.toFixed(2) + ' ' + toCurrency;
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+});
